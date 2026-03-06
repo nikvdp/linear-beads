0c5fe05117cdfe859cc342e64a56f31e45dc9b3c
Nik V <github@nikvdp.com>
2026-03-06 19:33:19 +0700

    Format database helper after project-wide prettier run
    - applied prettier normalization to looksLikeUuid in database helper
    
    This keeps formatting consistent with repository style checks.

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1905,9 +1905,7 @@ export function cacheLabel(id: string, name: string, teamId?: string): void {
 }
 
 function looksLikeUuid(value: string): boolean {
-  return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(
-    value
-  );
+  return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(value);
 }
 
 /**
